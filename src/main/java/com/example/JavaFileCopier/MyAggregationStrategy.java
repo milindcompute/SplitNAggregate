f8a5e473ca4a0f7056a0f43d85f9ba633a25c98c
milind.compute@gmail.com <milind.compute@gmail.com>
2019-02-27 07:01:19 +0530

    add

---
+++ src/main/java/com/example/JavaFileCopier/MyAggregationStrategy.java
@@ -0,0 +1,22 @@
+package com.example.JavaFileCopier;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.processor.aggregate.AggregationStrategy;
+
+public class MyAggregationStrategy implements AggregationStrategy {
+
+	public Exchange aggregate(Exchange oldExchange, Exchange newExchange) {
+		if (oldExchange == null) {
+			return newExchange;
+		 }
+		 String oldBody = oldExchange.getIn()
+		 .getBody(String.class);
+		 String newBody = newExchange.getIn()
+		 .getBody(String.class);
+		 System.out.println("Recieved ="+newBody);
+		 String body = oldBody + newBody;
+		 System.out.println("body ="+body);
+		 oldExchange.getIn().setBody(body);
+		 return oldExchange;
+		 } 
+}
